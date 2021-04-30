b7f19a36029c1c56ef78712d2c2a2dc7c3553e60
Reem Hazza3 <reemhazzaa4@gmail.com>
2021-04-30 10:00:45 +0200

    Create extentions.kt

---
+++ app/src/main/java/com/reemHazzaa/jobsapp/utils/extentions.kt
@@ -0,0 +1,11 @@
+package com.reemHazzaa.jobsapp.utils
+
+import android.view.View
+
+fun View.makeVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.makeGone() {
+    this.visibility = View.GONE
+}
