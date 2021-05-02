17d8f69788be4eb4675c210347a220edf731b39b
Reem Hazza3 <reemhazzaa4@gmail.com>
2021-05-02 12:03:14 +0200

    Update JobsDao.kt

--- app/src/main/java/com/reemHazzaa/jobsapp/screens/jobsList/data/room/JobsDao.kt
+++ app/src/main/java/com/reemHazzaa/jobsapp/screens/jobsList/data/room/JobsDao.kt
@@ -1,15 +1,15 @@
 package com.reemHazzaa.jobsapp.screens.jobsList.data.room
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface JobsDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertJobs(newJobs: JobsEntity)
+
+    @Update
+    suspend fun updateJobs(jobs: JobsEntity)
 
     @Query("SELECT * FROM TABLE_JOBS ORDER BY id ASC")
     fun readJobs(): Flow<List<JobsEntity>>
